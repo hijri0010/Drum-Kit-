6dcb542e6192340e16a8c0d77124e776daca2932
hijri0010 <132105423+hijri0010@users.noreply.github.com>
2023-12-10 17:09:35 +0700

    Add Files

---
+++ index.js
@@ -0,0 +1,48 @@
+function handleclick(){
+    // alert("I got clicked!");  
+    const audio = new Audio("./sounds/tom-4.mp3")
+    audio.play();  
+    this.style.color = "white";
+}
+
+const buttons = document.querySelectorAll("button");
+
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", function(){
+        const instrumentButton = this.innerHTML;
+    switch (instrumentButton) {
+        case "w":
+            const tom1 = new Audio("./sounds/tom-1.mp3")
+            tom1.play();
+            break;  
+        case "a":
+            const tom2 = new Audio("./sounds/tom-2.mp3")
+            tom2.play();
+            break;  
+        case "s":
+            const tom3 = new Audio("./sounds/tom-3.mp3")
+            tom3.play();
+            break;  
+        case "d":
+            const tom4 = new Audio("./sounds/tom-4.mp3")
+            tom4.play();
+            break;  
+        case "j":
+            const snare = new Audio("./sounds/snare.mp3")
+            snare.play();
+            break;  
+        case "k":
+            const crash = new Audio("./sounds/crash.mp3")
+            crash.play();
+            break;
+        case "l":
+            const kickbass = new Audio("./sounds/kick-bass.mp3")
+            kickbass.play();
+            break;  
+        default: console.log(instrumentButton);
+    }
+
+    });
+
+}
+
